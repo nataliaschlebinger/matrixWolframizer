8c0979965d5add81546a47bf240f3b1a41ae674d
Natalia Schlebinger <natalia.schlebinger@gmail.com>
2023-09-13 03:09:15 -0300

    Apply formatting

--- main.js
+++ main.js
@@ -1,32 +1,28 @@
+let width = 4,
+    height = 3;
 
-let width=4, height=3;
-
-let posx=0, posy=0;
+let posx = 0,
+    posy = 0;
 
 document.body.onload = loadExample;
 
-document.addEventListener("keydown",(e)=>{
-
-    if(!e.ctrlKey){
-        switch (e.key){
+document.addEventListener("keydown", e => {
+    if (!e.ctrlKey) {
+        switch (e.key) {
             case "ArrowUp":
-                if(posy>0)
-                    posy--;
+                if (posy > 0) posy--;
                 myfocus();
                 break;
             case "ArrowDown":
-                if(posy<height-1)
-                    posy++;
+                if (posy < height - 1) posy++;
                 myfocus();
                 break;
             case "ArrowRight":
-                if(posx<width-1)
-                    posx++;
+                if (posx < width - 1) posx++;
                 myfocus();
                 break;
             case "ArrowLeft":
-                if(posx>0)
-                    posx--;
+                if (posx > 0) posx--;
                 myfocus();
                 break;
             case "Enter":
@@ -36,77 +32,72 @@ document.addEventListener("keydown",(e)=>{
     }
 });
 
-function myfocus(){
-    document.getElementById("cell-"+posx+"-"+posy).focus();
+function myfocus() {
+    document.getElementById("cell-" + posx + "-" + posy).focus();
 }
 
-function makeNewCell(x, y){
-    const newInput=document.createElement("input");
-    const filling=document.getElementById("filling").value;
+function makeNewCell(x, y) {
+    const newInput = document.createElement("input");
+    const filling = document.getElementById("filling").value;
 
-    newInput.setAttribute("id","cell"+"-"+x+"-"+y);
-    newInput.setAttribute("size",2);
+    newInput.setAttribute("id", "cell" + "-" + x + "-" + y);
+    newInput.setAttribute("size", 2);
     newInput.setAttribute("value", filling);
-    newInput.addEventListener("click",(a)=>{
+    newInput.addEventListener("click", _ => {
         //closure-style
-        posx=x;
-        posy=y;
+        posx = x;
+        posy = y;
     });
     return newInput;
 }
 
-function loadExample(){
-
+function loadExample() {
     const matrix = document.getElementById("matrix");
 
-   
-    for(let i=0; i<height; i++){
+    for (let i = 0; i < height; i++) {
         const newDiv = document.createElement("div");
-        newDiv.setAttribute("id", "div"+i);
+        newDiv.setAttribute("id", "div" + i);
 
-        for(let j=0; j<width; j++){
-            const newInput= makeNewCell(j,i);
+        for (let j = 0; j < width; j++) {
+            const newInput = makeNewCell(j, i);
 
             newDiv.appendChild(newInput);
         }
         matrix.appendChild(newDiv);
     }
 
-    let aux=0;
-    for(let i=0; i<height; i++)
-        for(let j=0; j<width; j++){
-            document.getElementById("cell-"+j+"-"+i).setAttribute("value", aux);
+    let aux = 0;
+    for (let i = 0; i < height; i++)
+        for (let j = 0; j < width; j++) {
+            document.getElementById("cell-" + j + "-" + i).setAttribute("value", aux);
             aux++;
         }
-    
+
     updateDimensions();
     wolframize();
     myfocus();
-
 }
 
-function newRow(){
+function newRow() {
     const matrix = document.getElementById("matrix");
-   
 
     const newDiv = document.createElement("div");
-    newDiv.setAttribute("id", "div"+(height));
+    newDiv.setAttribute("id", "div" + height);
 
-    for(let i=0; i<width; i++){
-        const newInput=makeNewCell(i,height);
+    for (let i = 0; i < width; i++) {
+        const newInput = makeNewCell(i, height);
         newDiv.appendChild(newInput);
     }
     matrix.appendChild(newDiv);
     height++;
 
     updateDimensions();
-
 }
 
-function deleteRow(){
-    if(height>1){
-        document.getElementById("div"+(height-1)).remove();
-        if(posy==height-1){
+function deleteRow() {
+    if (height > 1) {
+        document.getElementById("div" + (height - 1)).remove();
+        if (posy == height - 1) {
             posy--;
             myfocus();
         }
@@ -115,11 +106,10 @@ function deleteRow(){
     updateDimensions();
 }
 
-function newCol(){
-
-    for(let i=0; i<height; i++){
-        const newInput=makeNewCell(width, i);
-        document.getElementById("div"+i).appendChild(newInput);
+function newCol() {
+    for (let i = 0; i < height; i++) {
+        const newInput = makeNewCell(width, i);
+        document.getElementById("div" + i).appendChild(newInput);
     }
 
     width++;
@@ -127,11 +117,11 @@ function newCol(){
     updateDimensions();
 }
 
-function deleteCol(){
-    if(width>1){
-        for(let i=0; i<height; i++)
-            document.getElementById("cell-"+(width-1)+"-"+i).remove();
-        if(posx==width-1){
+function deleteCol() {
+    if (width > 1) {
+        for (let i = 0; i < height; i++)
+            document.getElementById("cell-" + (width - 1) + "-" + i).remove();
+        if (posx == width - 1) {
             posx--;
             myfocus();
         }
@@ -140,27 +130,26 @@ function deleteCol(){
     updateDimensions();
 }
 
-function updateDimensions(){
-    document.getElementById("dimensions").innerHTML="The dimensions of your matrix are "+height+"x"+width;
+function updateDimensions() {
+    document.getElementById("dimensions").innerHTML =
+        "The dimensions of your matrix are " + height + "x" + width;
 }
 
-function wolframize(){
-    let out="{";
-    for(let i=0; i<height; i++){
-        out+="{";
-        for(let j=0; j<width; j++){
-            out+= document.getElementById("cell"+"-"+j+"-"+i).value+", ";
+function wolframize() {
+    let out = "{";
+    for (let i = 0; i < height; i++) {
+        out += "{";
+        for (let j = 0; j < width; j++) {
+            out += document.getElementById("cell" + "-" + j + "-" + i).value + ", ";
         }
-        out=out.substring(0, out.length-2);
-        out+="}, ";
+        out = out.substring(0, out.length - 2);
+        out += "}, ";
     }
-    out=out.substring(0, out.length-3);
-    out+="}}";
-    document.getElementById("output").innerHTML=out;
+    out = out.substring(0, out.length - 3);
+    out += "}}";
+    document.getElementById("output").innerHTML = out;
 }
 
-function mycopy(){
-    navigator.clipboard.writeText(
-        document.getElementById("output").innerHTML
-    );
+function mycopy() {
+    navigator.clipboard.writeText(document.getElementById("output").innerHTML);
 }
